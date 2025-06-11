5cbe987fe38c1115d00cbd2d12b84373919d4fd9
Tzvi Rolnik <tzvirol@std.hac.ac.il>
2025-06-11 19:53:55 +0300

    i am starting to do the Design path Factory

---
+++ include/GameObject/Factory.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <functional>
+#include <SFML/Graphics.hpp>
+
+#include "GameObject/Object.h"
+#include "GameObject/MovingObject/MovingObject.h"
+#include "GameObject/StaticObject/StaticObject.h"
+#include "GameObject/Images/ImagesObject.h"
+
+class Factory {
+public:
+    using CreatorFunc = std::function<std::unique_ptr<Object>(sf::Vector2f, const ImagesObject&)>;
+
+    static std::unique_ptr<Object> create(char symbol, sf::Vector2f location, const ImagesObject& images);
+    static bool registerIt(char symbol, CreatorFunc func);
+
+private:
+    static std::map<char, CreatorFunc>& getMap();
+};
