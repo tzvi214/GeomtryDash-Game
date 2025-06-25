f95f55d5fad256e1d3f6fd30c4e9a61f1805747a
Tzvi Rolnik <tzvirol@std.hac.ac.il>
2025-06-25 16:53:52 +0300

    i finshet with the sort sound in class sound

--- include/GUI/Sound.h
+++ include/GUI/Sound.h
@@ -6,9 +6,12 @@
 
 enum class SoundType// sort sound
 {
-    HitObstacle,
-    Jump,
-    Explosion
+   click,
+   finishedLevel,
+   gameOver,
+   notification,
+   touchGift,
+   unlock
 };
 
 enum class MusicType// long sound 
@@ -22,11 +25,17 @@ class Sound
 public:
     
     static void playMusic(MusicType type, float valume = 75.f);
+    static void playSound(SoundType type, float volume = 75.f);
 private:
-    static void playMusic(sf::Music&, float);
     static bool m_loaded;
     static bool initialize();
+     //-- music ---
+    static void playMusic(sf::Music&, float);
     static std::map<MusicType, sf::Music> m_musicMap;
     static void closeAllMusic();
+    // --- sound ---
+    static std::map<SoundType, sf::SoundBuffer> m_soundBuffers;
+    static std::vector<sf::Sound> m_sounds;
+    static const  int m_maxSound = 20;
 
 };
--- src/GUI/Sound.cpp
+++ src/GUI/Sound.cpp
@@ -1,8 +1,9 @@
 #include "GUI/Sound.h"
 #include <stdexcept>
+
 std::map<MusicType, sf::Music> Sound::m_musicMap;
-
-
+std::map<SoundType, sf::SoundBuffer> Sound::m_soundBuffers;
+std::vector<sf::Sound> Sound::m_sounds;
 bool Sound::m_loaded = [] { return Sound::initialize(); }();
 
 bool Sound::initialize()
@@ -11,13 +12,31 @@ bool Sound::initialize()
 
     if (m_initialized)
         return false;
-    
+    // Load music
     if (!m_musicMap[MusicType::MenuSound].openFromFile("MenuSound.mp3"))
         throw std::runtime_error("Failed to load MenuSound.mp3");
 
     if (!m_musicMap[MusicType::GameSound].openFromFile("GameSound.mp3"))
         throw std::runtime_error("Failed to load GameSound.mp3");
+    // Load short sounds
+    if (!m_soundBuffers[SoundType::unlock].loadFromFile("unlock.wav"))
+        throw std::runtime_error("Failed to load unlock.wav");
 
+    if (!m_soundBuffers[SoundType::touchGift].loadFromFile("touchGift.wav"))
+        throw std::runtime_error("Failed to load touchGift.wav");
+
+    if (!m_soundBuffers[SoundType::notification].loadFromFile("notification.wav"))
+        throw std::runtime_error("Failed to load notification.wav");
+
+    if (!m_soundBuffers[SoundType::gameOver].loadFromFile("gameOver.wav"))
+        throw std::runtime_error("Failed to load gameOver.wav");
+
+    if (!m_soundBuffers[SoundType::finishedLevel].loadFromFile("finishedLevel.wav"))
+        throw std::runtime_error("Failed to load finishedLevel.wav");
+
+    if (!m_soundBuffers[SoundType::click].loadFromFile("click.mp3"))
+        throw std::runtime_error("Failed to load click.mp3");
+   
     m_initialized = true;
     return true;
 }
@@ -42,9 +61,45 @@ void Sound::playMusic(MusicType type, float valume)
    playMusic(it->second, valume);
 }
 
+
 void Sound::playMusic(sf::Music& music, float valume)
 {
     music.setLoop(true);
     music.setVolume(valume);
     music.play();
 }
+
+void Sound::playSound(SoundType type, float volume)
+{
+    auto it = m_soundBuffers.find(type);
+    if (it == m_soundBuffers.end())
+        throw std::runtime_error("Failed to found sound type");
+
+
+    m_sounds.erase(
+        std::remove_if(m_sounds.begin(), m_sounds.end(),
+            [](const sf::Sound& s) { return s.getStatus() == sf::Sound::Stopped; }),
+        m_sounds.end());
+
+    for (auto& s : m_sounds)
+    {
+        if (s.getStatus() != sf::Sound::Playing)
+        {
+            s.setBuffer(it->second);
+            s.setVolume(volume);
+            s.play();
+            return;
+        }
+    }
+
+    
+  
+    if (m_sounds.size() < m_maxSound)
+    {
+        m_sounds.emplace_back(); 
+        m_sounds.back().setBuffer(it->second);
+        m_sounds.back().setVolume(volume);
+        m_sounds.back().play();
+    }
+}
+
